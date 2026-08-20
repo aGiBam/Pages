e0cbda856e3075517b665b4f907d304b385d5195
MohamedAgiba <mohamed.agiba@cic.ae>
2026-08-20 08:54:33 +0300

    feat: set up bootstrap and add image pagination gallery
    Update bootstrap asset paths in styles and angular config
    Replace default Angular starter template with pagination navigation
    Add sample image data array to the root App component
    Remove unused RouterOutlet imports and code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('pages');
+  images = [
+    {
+      title: 'Beach',
+      url: 'https://plus.unsplash.com/premium_photo-1682629632657-4ac307921295?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmVhY2h8ZW58MHx8MHx8fDA%3D'
+    },
+    {
+      title: 'Mountains',
+      url: 'https://plus.unsplash.com/premium_photo-1690441053731-600981d923ab?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDF8NnNNVmpUTFNrZVF8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+      title: 'Nature',
+      url: 'https://plus.unsplash.com/premium_photo-1690441053731-600981d923ab?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDF8NnNNVmpUTFNrZVF8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+      title: 'flower',
+      url: 'https://images.unsplash.com/photo-1471899236350-e3016bf1e69e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvd2VyfGVufDB8fDB8fHww'
+    }
+  ]
 }

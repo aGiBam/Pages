86e168c99abf181aa7a1fad841cf467152796bc2
MohamedAgiba <mohamed.agiba@cic.ae>
2026-08-20 11:00:02 +0300

    feat(appClass): add configurable bg color, fix duplicate click
    Update the Class directive to accept a backgroundColor input, replacing the hardcoded orange background. Remove the duplicate (click) event handler on pagination anchor tags, and update the app template to pass a red background color to the appClass directive. Also comment out the debug console log.

--- src/app/class.ts
+++ src/app/class.ts
@@ -1,12 +1,13 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appClass]',
 })
 export class Class {
-  constructor(private element: ElementRef ) {
-    this.element.nativeElement.style.backgroundColor = 'orange';
-    console.log('Class Directive used !');
+  @Input() backgroundColor: string;
 
+  constructor(private element: ElementRef ) {
+    this.element.nativeElement.style.backgroundColor = this.backgroundColor;
+    // console.log('Class Directive used !');
   }
 }
